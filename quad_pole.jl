8c252fe71e717083773d848c6a7fb0d17afcef34
Cecil <zfuaj@connect.ust.hk>
2021-08-09 18:27:32 +0800

    add project.toml

--- quad_pole.jl
+++ quad_pole.jl
@@ -1,6 +1,5 @@
-using Base: Float64
-using TrajectoryOptimization: convert
-using MeshCatMechanisms: findbody, findjoint, configuration, velocity
+using TrajectoryOptimization
+using MeshCatMechanisms
 using RigidBodyDynamics
 using MeshCat
 using MeshCatMechanisms
@@ -8,9 +7,7 @@ using StaticArrays
 using Rotations
 using LinearAlgebra
 using ForwardDiff
-using MeshCatMechanisms
 
-using TrajectoryOptimization
 using Altro
 using RobotDynamics
 using PyPlot
@@ -88,9 +85,9 @@ conSet = ConstraintList(n,m,N)
 x_bnd = [fill(Inf, 7);0.1;0.1;fill(Inf, 8)]
 bnd = BoundConstraint(n, m, x_min=-x_bnd, x_max=x_bnd)
 goal = GoalConstraint(xf)
-# waypoint = GoalConstraint([3,2,0,0,0,0,0,0], [1,2])
+waypoint = GoalConstraint([[0.9330127, 0.25, 0.25, 0.0669873]; zeros(13)], 1:4)
 add_constraint!(conSet, bnd, 1:N-1)
-# add_constraint!(conSet, waypoint, 500)
+add_constraint!(conSet, waypoint, 200)
 add_constraint!(conSet, goal, N)
 
 prob = Problem(quad, obj, xf, tf, x0=x0, constraints=conSet)
@@ -118,9 +115,13 @@ animation = Animation(mvis, ts, x_traj)
 setanimation!(mvis, animation)
 
 pygui(true)
-theta_traj = [x[8:9]' for x in X]
+theta_traj = [rad2deg.(x[8:9]') for x in X]
 theta_traj = vcat(theta_traj...)
-plot(ts', theta_traj)
+plot(ts, theta_traj)
+
+ori_traj = [rad2deg(RotXYZ(UnitQuaternion(x[1:4]...))[1]) for x in X]
+ori_traj = vcat(ori_traj...)
+plot(ts, ori_traj)
 
 
 # set_configuration!(quad.statecache[Float64], findjoint(quad.mech, "base_to_world"), [1, 2., 3., 4., .1, .0, .0])
